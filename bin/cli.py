27a6648c1c3ff59d4a5dbe11c7ebada4bb515a6c
Roger Ostrander <reostra@reddit.com>
2013-06-14 15:01:02 -0700

    CLI for me to use while debugging/monitoring

---
+++ bin/cli.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import code
+import logging
+import readline
+import sys
+
+sys.path.append(".")
+sys.path.append("./chromabot")
+
+from config import Config
+from db import *
+from utils import *
+
+def all(cls, **kw):
+    return query(cls, **kw).all()
+
+def by_id(cls, id):
+    return query(cls, id=id).first()
+
+def commit():
+    return sess.commit()
+
+def first(cls, **kw):
+    return query(cls, **kw).first()
+
+def query(cls, **kw):
+    return sess.query(cls).filter_by(**kw)
+
+def main():
+    global sess
+    logging.basicConfig(level=logging.DEBUG)
+    
+    # Some handy locals
+    config = Config()
+    dbconn = DB(config)
+    sess = dbconn.session()
+    
+    
+    vars = globals().copy()
+    vars.update(locals())
+    shell = code.InteractiveConsole(vars)
+    shell.interact("Chromabot CLI ready")
+
+if __name__ == "__main__":
+    main()
